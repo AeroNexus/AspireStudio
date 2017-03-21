00e5f55e6bc4e3f89b627d194fd55303c100d019
Roderick Green <roderick.green@orbitlogic.com>
2017-03-21 11:01:16 -0600

    revert serialization of access to MessageHandler::Parse

--- Core/MessageHandler.cs
+++ Core/MessageHandler.cs
@@ -47,18 +47,18 @@ namespace Aspire.Core
 
 		public void Close()	{}
 
-		static readonly SecTime maxWait = SecTime.Milliseconds(250);
-    
-    /// <summary>
-    /// Field messages sent by other components to this Application-based component.
-    /// Blocks on the asynchronous listen transport for lesser of executePeriod/0.25s,
-    /// waiting for messages to arrive.
-    /// When they do, select the appropriate protocol and give the network buffer to
-    /// the protocol for appropriate action. Calls Execute() via a Timer. Blocks while
-    /// app has not closed
-    /// </summary>
-    /// <param name="application"></param>
-    public void FieldMessages()
+		static SecTime maxWait = SecTime.Milliseconds(250);
+
+		/// <summary>
+		/// Field messages sent by other components to this Application-based component.
+		/// Blocks on the asynchronous listen transport for lesser of executePeriod/0.25s,
+		/// waiting for messages to arrive.
+		/// When they do, select the appropriate protocol and give the network buffer to
+		/// the protocol for appropriate action. Calls Execute() via a Timer. Blocks while
+		/// app has not closed
+		/// </summary>
+		/// <param name="application"></param>
+		public void FieldMessages()
 		{
 			var now = new SecTime();
 
@@ -76,32 +76,22 @@ namespace Aspire.Core
 			}
 		}
 
-    /// <summary>
-    /// Serialize access to (all) message handlers to avoid cross-thread data access.
-    /// 
-    /// I now consider this parseMutex a temporary workaround until I understand
-    /// where the cross thread access is. I previously thought it was in the
-    /// variable mappings but that isn't the case.
-    /// </summary>
-    static readonly object parseMutex = new object();
-    public bool Parse(byte[] buffer, int length, Message parseHeader)
+
+		public bool Parse(byte[] buffer, int length, Message parseHeader)
 		{
-      lock (parseMutex)
-      {
-        byte protocolId = buffer[0];
-        if (protocolId < mProtocols.Count)
-        {
-          // Temporarily snag Monarch Local protocol. Need to move Aspire protocols up to 3,4,5
-          if (protocolId == 2 && buffer[1] >= 32 && buffer[1] <= 36)
-            protocolId = (byte)ProtocolId.Monarch;
-          Protocol protocol = mProtocols[protocolId];
-          if (protocol != null)
-            return protocol.Parse(buffer, length, parseHeader);
-        }
-        else if (HandleUnknownProtocol != null)
-          HandleUnknownProtocol(buffer, EventArgs.Empty);
-        return false;
-      }
+			byte protocolId = buffer[0];
+			if (protocolId < mProtocols.Count)
+			{
+				// Temporarily snag Monarch Local protocol. Need to move Aspire protocols up to 3,4,5
+				if (protocolId == 2 && buffer[1] >= 32 && buffer[1] <= 36)
+					protocolId = (byte)ProtocolId.Monarch;
+				Protocol protocol = mProtocols[protocolId];
+				if (protocol != null)
+					return protocol.Parse(buffer, length, parseHeader);
+			}
+			else if (HandleUnknownProtocol != null)
+				HandleUnknownProtocol(buffer, EventArgs.Empty);
+			return false;
 		}
 
 		byte[] pollBuffer = new byte[64*1024];
